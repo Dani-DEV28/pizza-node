0b66619dd8f3e9c42f199d29ec94a5f7bc218498
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-01-28 13:57:37 -0800

    fixed

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req,res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
+app.get('/thankyou', (req,res)=>{
+    res.sendFile(`${import.meta.dirname}/views/thankyou.html`);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running at https://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
 });
